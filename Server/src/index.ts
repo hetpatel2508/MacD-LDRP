451881405a274fdbab62e8e7b3bddbfcc5c14a5b
Patel Het <125752460+hetpatel2508@users.noreply.github.com>
2025-01-20 13:00:08 +0530

    .

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -9,7 +9,7 @@ const app: Express = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
